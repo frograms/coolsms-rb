592133c226fd411b945c0110a4c30578a052b029
scott <scott@watcha.com>
2022-03-16 18:03:23 +0900

    Add options parameter to faraday_json_parser

--- lib/coolsms/rest_api/faraday_json_parser.rb
+++ lib/coolsms/rest_api/faraday_json_parser.rb
@@ -2,6 +2,6 @@ FaradayMiddleware::ParseJson.dependency do
   require 'oj' unless defined?(::Oj)
 end
 
-FaradayMiddleware::ParseJson.define_parser do |body|
-  ::Oj.load(body || '')
+FaradayMiddleware::ParseJson.define_parser do |body, parser_options|
+  ::Oj.load(body || '', parser_options)
 end
